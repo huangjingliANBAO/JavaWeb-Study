36f93f42aad57152f2aa0d85e97edc0378bb790f
黄敬理 <jingliqaq@gmail.com>
2019-07-22 18:47:34 +0800

    add checkcode

--- web/WEB-INF/classes/src/UserLogin/LoginServlet.java
+++ web/WEB-INF/classes/src/UserLogin/LoginServlet.java
@@ -17,16 +17,20 @@ public class LoginServlet extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String checkCode = request.getParameter("check_code");
+        String savedCode = (String) request.getSession().getAttribute("check_code");
         PrintWriter pw = response.getWriter();
         //假设正确的用户名 是itcast 密码是123
-        if (("itcast").equals(username) && ("123").equals(password)){
+        if (("itcast").equals(username) && ("123").equals(password) && checkCode.equals(savedCode)){
             User user = new User();
             user.setUsername(username);
             user.setPassword(password);
             request.getSession().setAttribute("user",user);
             response.sendRedirect("/JavaWeb/IndexServlet");
-        }else {
+        }else if (checkCode.equals(savedCode)){
             pw.print("用户名或密码错误，登录失败！");
+        }else {
+            pw.print("验证码错误");
         }
     }
     protected void doPost(HttpServletRequest request,
